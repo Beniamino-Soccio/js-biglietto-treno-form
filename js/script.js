7da6ac827095d49b86a77824d9914268f44d32ce
Beniamino-Soccio <beniamino43@gmail.com>
2024-08-01 16:20:11 +0200

    recuperati i valori e stampati in console

--- js/script.js
+++ js/script.js
@@ -14,3 +14,19 @@ console.log('JS OK')
 6.dopo aver inserito un form in pagina svolgo i passaggi dal 2 al 4
 7.stampo in pagina il recap dei datiinseriti(km,età) e il prezzo del biglietto (con 2 decimali)
 */
+
+const button = document.getElementById('confirm');
+console.log('bottone conferma', typeof button);
+
+const kmField = document.getElementById('user-km');
+console.log(kmField);
+const ageField = document.getElementById('user-age');
+console.log(ageField);
+
+button.addEventListener('click', function(){
+    //recupero i valori dal form
+    const kmValue = parseInt(kmField.value.trim());
+    const ageValue = ageField.value.trim();
+    //stampo i valori in console
+    console.log('km ' + kmValue, 'età ' + ageValue);
+})
