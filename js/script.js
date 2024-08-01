46dad2632f8cdcd9942c5506153107cfbcee7715
Beniamino-Soccio <beniamino43@gmail.com>
2024-08-01 16:19:21 +0200

    ex. phases

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 // prova collegamento file .js
 console.log('JS OK')
+
+//pasaggi traccia milestone1
+/*
+1.preparo eventuali variabili che so che mi serviranno sicuramente dopo
+2.scrivere le informazioni (km,età) nell'apposita input in pagina
+3.grazie al bottone raccolgo le informazioni (km, età)
+4.calcolo il prezzo del biglietto in base a quanti anni ha l'utente
+5.stampo in console il prezzo del biglietto
+*/
+//passaggi milestone2
+/*
+6.dopo aver inserito un form in pagina svolgo i passaggi dal 2 al 4
+7.stampo in pagina il recap dei datiinseriti(km,età) e il prezzo del biglietto (con 2 decimali)
+*/
